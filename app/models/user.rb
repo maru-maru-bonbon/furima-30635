9e21203f412aa3d9c7e433865029b58dba6ec1a2
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2020-10-27 18:53:50 +0900

    birthdayカラムのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,9 @@ class User < ApplicationRecord
          validates :nickname, presence: true
          validates :last_name, presence: true
          validates :first_name, presence: true
-         validates :last_katakana, presnece: true
+         validates :last_katakana, presence: true
          validates :first_katakana, presence: true
-         
+         validates :birthday, presence: true
+
+
 end
