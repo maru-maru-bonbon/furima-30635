51db0a6cbfa8184c6983847d8ac9b946ecc3353a
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2020-10-28 17:21:24 +0900

    userモデルの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX
+         
          validates :nickname, presence: true
+      with_options presence: true, format: { with: /\A[ぁ-んぁ一-龥]+\z/, message: '全角文字を使用してください' } do
          validates :last_name, presence: true
          validates :first_name, presence: true
+      end
+      
+      with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角文字を使用してください' } do
          validates :last_katakana, presence: true
          validates :first_katakana, presence: true
+      end
          validates :birthday, presence: true
 
 
