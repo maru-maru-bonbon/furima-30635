003a4bd1c0ca0b9d97890c29dfe4be4c0fccaea4
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2020-11-04 02:52:35 +0900

    itemモデルにバリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,14 @@
 class Item < ApplicationRecord
-  validates :image, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_time
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :shipping_area
+  belongs_to :user
+
+
+  validates :image, presence: true, unless: :was_attached?
   validates :name, presence: true  
   validates :description, presence: true
   validates :category_id, presence: true
@@ -8,4 +17,12 @@ class Item < ApplicationRecord
   validates :shipping_area_id, presence: true
   validates :delivery_time_id, presence: true
   validates :selling_price, presence: true
+  with_options format: { with: /\A[0-9]+\z/} do
+    validates :selling_price, numericality: {only_integer: true, greater_than:299,less_than: 9999999}
+  end
+  validates :category_id, :condition_id, :shipping_area_id, :shipping_charge_id, :delivery_time_id, numericality: { other_than: 1 } 
+  has_one_attached :image
+  def was_attached?
+    self.image.attached?
+  end
 end
