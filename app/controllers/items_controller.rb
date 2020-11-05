ef34b57d8f0c551478389da89f7a3bdb71e34830
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2020-11-05 17:19:39 +0900

    コントローラにdestroyメソッドを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!,only: [:new, :edit]
-  before_action :set_item,only: [:show, :edit, :update]
+  before_action :set_item,only: [:show, :edit, :update ,:destroy]
 
   def index
     @items =Item.all.order("created_at DESC")
@@ -36,6 +36,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
   private
 
