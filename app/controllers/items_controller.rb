636711400368eecf01ef9ab7b3bd252001934fb5
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2020-11-05 16:30:26 +0900

    指定箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!,only: [:new, :edit]
   before_action :set_item,only: [:show, :edit, :update]
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
-
   def index
     @items =Item.all.order("created_at DESC")
   end
@@ -45,6 +41,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_area_id, :delivery_time_id, :shipping_charge_id, :selling_price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 
