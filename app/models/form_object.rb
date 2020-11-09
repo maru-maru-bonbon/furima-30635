8fb96b3f9b59c7fde90c082acf970d94c62fb9ad
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2020-11-09 18:43:56 +0900

    form_objectモデルをクレジットカードの情報を保存しないように修正、purchase_detailをpurchase_deatail_idに修正、purchasedetailとShippingAddressの読み込む順番を変更

--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -1,20 +1,20 @@
 class FormObject
   include ActiveModel::Model
-  attr_accessor :number, :exp_month, :exp_year, :cvc, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :purchase_detail, :token, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number, :purchase_detail_id, :token, :user_id, :item_id
 
   with_options presence: true do
     validates :prefecture_id
     validates :municipality
     validates :address
     validates :phone_number
-    validates :purchase_detail
+    validates :purchase_detail_id
 
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
   validates :prefecture_id, numericality: { other_than: 1}
 
   def save
-    ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_detail_id:purchase_detail.id)
-    PurchaseDetail.create(item_id: item.id, user_id: current_user.id)
+    purchasedetail = PurchaseDetail.create(item_id: item_id, user_id: user_id)
+    ShippingAddress.create!(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_detail_id:purchasedetail.id)
   end
 end
