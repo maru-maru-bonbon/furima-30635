f11ae151e507ee123482bec9f6dbb23acde7ce0f
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2020-11-09 11:44:43 +0900

    purchase_detailモデルにアソシエーションを記述

--- app/models/purchase_detail.rb
+++ app/models/purchase_detail.rb
@@ -1,2 +1,7 @@
 class PurchaseDetail < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user 
+  belongs_to :item
+  has_one :shipping_address
+  belongs_to_active_hash :shipping_area
 end
---
+++ spec/requests/purchase_details_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "PurchaseDetails", type: :request do
+
+end
