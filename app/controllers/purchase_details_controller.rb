4184550bf0deee5515914811559e5ae9941b15b4
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2020-11-09 18:49:23 +0900

    エラー修正のためにcreateにfindを記述、ログアウト状態で購入画面に入るとログイン画面に遷移するように修正

--- app/controllers/purchase_details_controller.rb
+++ app/controllers/purchase_details_controller.rb
@@ -1,14 +1,19 @@
 class PurchaseDetailsController < ApplicationController
+  before_action :authenticate_user!,only: [:index]
 
   def index
     @items = Item.find(params[:item_id])
     @form_object = FormObject.new
+    if current_user == @items.user.id
+      redirect_to root_path
+    end
   end
 
   def create
+    @items = Item.find(params[:item_id])
     @form_object = FormObject.new(purchase_details_params)
+    @form_object.save
     if @form_object.valid?
-      @form_object.save
       redirect_to root_path
     else
       render :index
@@ -19,6 +24,7 @@ class PurchaseDetailsController < ApplicationController
   private
 
   def purchase_details_params
-    params.require(:form_object).permit(:postal_code,:prefecture_id,:municipality,:address,:phone_number).merge(token: params[:token],user_id: current_user.id,item_id: params[:item_id])
+    params.require(:form_object).permit(:postal_code,:prefecture_id,:municipality,:address,:phone_number,:building_name).merge(token: params[:token],user_id: current_user.id,item_id: params[:item_id])
   end
+
 end
