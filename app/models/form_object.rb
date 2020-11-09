01165be67f8a0a6c01f9657a2ee20d3976c762e1
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2020-11-09 14:24:54 +0900

    form_objectのバリデーションを修正

--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -11,7 +11,7 @@ class FormObject
 
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
-  validates :prefecture, numericality: { other_than: 1}
+  validates :prefecture_id, numericality: { other_than: 1}
 
   def save
     ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address, building_name: building_name, phone_number: phone_number, purchase_detail_id:purchase_detail.id)
