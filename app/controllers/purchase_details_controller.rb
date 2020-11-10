a2be0c2faf526f3724c361f48de9797e44de00f8
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2020-11-10 19:10:59 +0900

    purchase_detailsコントローラに決済処理にバリデーションを行うように記述、加えて住所情報も購入の都度入力させるためにrequireを修正

--- app/controllers/purchase_details_controller.rb
+++ app/controllers/purchase_details_controller.rb
@@ -1,19 +1,19 @@
 class PurchaseDetailsController < ApplicationController
   before_action :authenticate_user!,only: [:index]
+  before_action :set_item,only: [:index,:create,:pay_item]
 
   def index
-    @items = Item.find(params[:item_id])
     @form_object = FormObject.new
-    if current_user == @items.user.id
+    if current_user.id == @items.user.id 
       redirect_to root_path
     end
   end
 
   def create
-    @items = Item.find(params[:item_id])
     @form_object = FormObject.new(purchase_details_params)
-    @form_object.save
     if @form_object.valid?
+      pay_item
+      @form_object.save
       redirect_to root_path
     else
       render :index
@@ -24,7 +24,21 @@ class PurchaseDetailsController < ApplicationController
   private
 
   def purchase_details_params
-    params.require(:form_object).permit(:postal_code,:prefecture_id,:municipality,:address,:phone_number,:building_name).merge(token: params[:token],user_id: current_user.id,item_id: params[:item_id])
+    params.permit(:postal_code,:prefecture_id,:municipality,:address,:phone_number,:building_name).merge(token: params[:token],user_id: current_user.id,item_id: params[:item_id])
   end
 
+  def set_item
+    @items = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @items.selling_price,
+      card: purchase_details_params[:token],    
+      currency: 'jpy' 
+    )                
+  end
+
+ 
 end
