ed3ecf7da318cf9b20563d0dec261135add38f32
maru-maru-bonbon <71998423+maru-maru-bonbon@users.noreply.github.com>
2020-11-10 19:12:40 +0900

    form_objectモデルにphone_numberのバリデーション追加、prefecture_idにメッセージ設定

--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -6,12 +6,13 @@ class FormObject
     validates :prefecture_id
     validates :municipality
     validates :address
-    validates :phone_number
-    validates :purchase_detail_id
+    validates :phone_number,  length: { maximum: 11 } 
+    validates :phone_number,  numericality: { only_integer: true }
+    validates :token
 
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
-  validates :prefecture_id, numericality: { other_than: 1}
+  validates :prefecture_id, numericality: { other_than: 1 ,message:  "Select"}
 
   def save
     purchasedetail = PurchaseDetail.create(item_id: item_id, user_id: user_id)
